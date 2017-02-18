f2b0f95ef7fd3bacd00fb1061277fe4e23639cf2
Powell <Melanie_Powell@gallup.com>
2017-02-18 14:11:18 -0600

    fixed merge conflicts

--- TeamOrganizr/client/main.js
+++ TeamOrganizr/client/main.js
@@ -4,20 +4,21 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 Template.register.events({
-	'submit form': function(event){
-		event.preventDefault();
-		var email = $('[name=email]').val();
-		var password = $('[name=password]').val();
-		Accounts.createUser({
-			email: email,
-			password: password
-		}, function(error){
-			if(error){
-				console.log(error.reason);
-			} else {
-				Router.go("home");
-			}
-	}
+  'submit form': function(event){
+    event.preventDefault();
+    var email = $('[name=email]').val();
+    var password = $('[name=password]').val();
+    Accounts.createUser({
+      email: email,
+      password: password
+    }, function(error){
+      if(error){
+        console.log(error.reason);
+      } else {
+        Router.go("home");
+      }
+    }
+  )}
 });
 
 Template.navigation.events({
