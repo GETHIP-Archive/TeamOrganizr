aefdd5acb7ccbc39ebb910ff25a7513a20ad8b4e
g_r_a_c_e <serixog865@ops.org>
2017-02-18 13:51:39 -0600

    registration

--- TeamOrganizr/client/main.js
+++ TeamOrganizr/client/main.js
@@ -26,6 +26,40 @@ Template.register.events({
 		event.preventDefault();
 		var email = $('[name=email]').val();
 		var password = $('[name=password]').val();
+		Accounts.createUser({
+			email: email,
+			password: password
+		}, function(error){
+			if(error){
+				console.log(error.reason);
+			} else {
+				Router.go("home");
+			}
+	}
+});
+
+Template.navigation.events({
+	'click .logout': function(event){
+		event.preventDefault();
+		Meteor.logout();
+		Router.go('login');
+	}
+});
+
+Template.login.events({
+	'submit form': function(event){
+		event.preventDefault();
+		var email = $('[name=email]').val();
+		var password = $('[name=password]').val();
+		Meteor.loginWithPassword(email, password);
+	}
+});
+
+Meteor.loginWithPassword(email, password, function(error){
+	if(error){
+		console.log(error.reason);
+	} else {
+		Router.go("home");
 	}
 });
 
