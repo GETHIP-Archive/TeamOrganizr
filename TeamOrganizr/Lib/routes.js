a0a0eb85727fc8e16286f394b060164be22a3db6
mrcopprhead <qwertyjcleveland@gmail.com>
2017-02-18 10:20:02 -0600

    added templates and deleted the old stuff

---
+++ TeamOrganizr/Lib/routes.js
@@ -0,0 +1,9 @@
+import { Meteor } from 'meteor/meteor';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+FlowRouter.route('/', {
+  name: "home",
+  action: function() {
+    BlazeLayout.render("content", {content: "dashboard"});
+  }
+});
