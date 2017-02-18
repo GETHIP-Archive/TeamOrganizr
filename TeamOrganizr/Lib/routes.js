687363bab207dfdd5d9c4b26b68bc72a5c401fbc
mrcopprhead <qwertyjcleveland@gmail.com>
2017-02-18 11:08:55 -0600

    I changed the content to home Changed the content to home

--- TeamOrganizr/Lib/routes.js
+++ TeamOrganizr/Lib/routes.js
@@ -1,9 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
 FlowRouter.route('/', {
   name: "home",
   action: function() {
-    BlazeLayout.render("content", {content: "dashboard"});
+    BlazeLayout.render("content", {content: "home"});
   }
 });
