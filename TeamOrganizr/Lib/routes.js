c2a9b5b5545b3f0935ed9e62f20182e96c5b816a
g_r_a_c_e <serixog865@ops.org>
2017-02-18 11:33:29 -0600

    register

--- TeamOrganizr/Lib/routes.js
+++ TeamOrganizr/Lib/routes.js
@@ -1,9 +1,25 @@
 import { Meteor } from 'meteor/meteor';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+
 FlowRouter.route('/', {
   name: "home",
   action: function() {
-    BlazeLayout.render("content", {content: "dashboard"});
+    BlazeLayout.render("content", {content: "home"});
+  }
+});
+
+FlowRouter.route('/', {
+  name: "about",
+  action: function() {
+    BlazeLayout.render("content", {content: "aboutPage"});
+  }
+});
+
+FlowRouter.route('/', {
+  name: "registration",
+  action: function() {
+    BlazeLayout.render("content", {content: "register"});
   }
 });
